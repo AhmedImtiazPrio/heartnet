359c396cba36375b45466f4b48e813e183a5ac0d
AhmedImtiazPrio <ahmed.imtiaz.prio@gmail.com>
2018-01-24 16:24:12 +0600

    linear phase filter design constraint added
    Former-commit-id: e8c083337db30d454421f60cbde25be0bac064db [formerly 88835f95ba5d8d5f6e9e6032cfa191bab642a0c7]
    Former-commit-id: 6c4bc28b796ca7ef7fedb04bb7106c66819b5484

--- codes/custom_layers.py
+++ codes/custom_layers.py
@@ -138,3 +138,148 @@ class Conv1D_zerophase(Layer):
         base_config = super(Conv1D_zerophase, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
+
+class Conv1D_zerophase_linear(Layer):
+
+    def __init__(self, filters, kernel_size, rank=1, strides=1, padding='valid', data_format='channels_last',
+                 dilation_rate=1, activation=None, use_bias=True, kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
+                 kernel_constraint=None, bias_constraint=None, **kwargs):
+        super(Conv1D_zerophase_linear, self).__init__(**kwargs)
+        self.rank = rank
+        self.filters = filters
+        if kernel_size % 2:
+            self.kernel_size = conv_utils.normalize_tuple(kernel_size / 2 + 1, rank, 'kernel_size')
+        else:
+            self.kernel_size = conv_utils.normalize_tuple(kernel_size / 2, rank, 'kernel_size')
+        self.strides = conv_utils.normalize_tuple(strides, rank, 'strides')
+        self.padding = conv_utils.normalize_padding(padding)
+        self.data_format = conv_utils.normalize_data_format(data_format)
+        self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, rank, 'dilation_rate')
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.input_spec = InputSpec(ndim=self.rank + 2)
+
+    def build(self, input_shape):
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs '
+                             'should be defined. Found `None`.')
+        input_dim = input_shape[channel_axis]
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=self.rank + 2,
+                                    axes={channel_axis: input_dim})
+        self.built = True
+
+    def call(self, inputs):
+
+        if self.kernel_size[0] % 2 == 0:
+            flipped = tf.reverse(self.kernel, axis=[0])
+        else:
+            flipped = tf.reverse(self.kernel[1:, :, :], axis=[0])
+        #         print (flipped)
+        conv_kernel = tf.concat([flipped, self.kernel], axis=0)
+        #         print (conv_kernel)
+
+        outputs = K.conv1d(
+            inputs,
+            conv_kernel,
+            strides=self.strides[0],
+            padding='same',
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate[0])
+        #         print tf.shape(outputs)
+        outputs = tf.reverse(outputs, axis=[1])
+        outputs = K.conv1d(
+            outputs,
+            conv_kernel,
+            strides=self.strides[0],
+            padding=self.padding,
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate[0])
+        #         print tf.shape(outputs)
+        outputs = tf.reverse(outputs, axis=[1])
+        if self.use_bias:
+            outputs = K.bias_add(
+                outputs,
+                self.bias,
+                data_format=self.data_format)
+
+        if self.activation is not None:
+            return self.activation(outputs)
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_last':
+            space = input_shape[1:-1]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_utils.conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding=self.padding,
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            return (input_shape[0],) + tuple(new_space) + (self.filters,)
+        if self.data_format == 'channels_first':
+            space = input_shape[2:]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_utils.conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding=self.padding,
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            return (input_shape[0], self.filters) + tuple(new_space)
+
+    def get_config(self):
+        config = {
+            'rank': self.rank,
+            'filters': self.filters,
+            'kernel_size': self.kernel_size,
+            'strides': self.strides,
+            'padding': self.padding,
+            'data_format': self.data_format,
+            'dilation_rate': self.dilation_rate,
+            'activation': activations.serialize(self.activation),
+            'use_bias': self.use_bias,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+            'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+            'kernel_constraint': constraints.serialize(self.kernel_constraint),
+            'bias_constraint': constraints.serialize(self.bias_constraint)
+        }
+        base_config = super(Conv1D_zerophase_linear, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
