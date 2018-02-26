d9488c71362bb781432cfaf473900d72b83ce139
AhmedImtiazPrio <ahmed.imtiaz.prio@gmail.com>
2018-02-26 17:00:19 +0600

    added default value for activation

--- codes/heartnet_v1.py
+++ codes/heartnet_v1.py
@@ -66,7 +66,7 @@ def write_meta(Y,log_dir):
     metadata_file.close()
     return os.path.join(log_dir, 'metadata.tsv')
 
-def heartnet(load_path,activation_function, bn_momentum=0.99, bias=False, dropout_rate=0.5, dropout_rate_dense=0.0,
+def heartnet(load_path,activation_function='relu', bn_momentum=0.99, bias=False, dropout_rate=0.5, dropout_rate_dense=0.0,
              eps=1.1e-5, kernel_size=5, l2_reg=0.0, l2_reg_dense=0.0,lr=0.0012843784, lr_decay=0.0001132885, maxnorm=10000.,
              padding='valid', random_seed=1, subsam=2, num_filt=(8, 4), num_dense=20,FIR_train=False):
 
