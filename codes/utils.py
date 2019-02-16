98966864c58052b20250bf93c016ab561e5f281f
Ahmed Imtiaz <ahmed.imtiaz.prio@gmail.com>
2019-02-16 11:52:48 +0600

    DenseNet test

---
+++ codes/utils.py
@@ -0,0 +1,178 @@
+from keras.layers.core import Dropout, Activation
+from keras.layers import Conv1D
+from keras.layers.pooling import AveragePooling1D
+from keras.layers.pooling import GlobalAveragePooling1D
+from keras.layers import Concatenate
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+import keras.backend as K
+
+def conv_factory(x, concat_axis=-1, nb_filter=16, kernel_size=5,
+                 dropout_rate=None, weight_decay=1E-4):
+    """Apply BatchNorm, Relu 3x3Conv1D, optional dropout
+    :param x: Input keras network
+    :param concat_axis: int -- index of contatenate axis
+    :param nb_filter: int -- number of filters
+    :param dropout_rate: float 0~1 -- dropout rate
+    :param weight_decay: int -- weight decay factor
+    :returns: keras network with b_norm, relu and Conv1D added
+    :rtype: keras network
+    """
+
+    x = BatchNormalization(axis=concat_axis,
+                           gamma_regularizer=l2(weight_decay),
+                           beta_regularizer=l2(weight_decay))(x)
+    x = Activation('relu')(x)
+    x = Conv1D(nb_filter, kernel_size=kernel_size,
+               kernel_initializer="he_uniform",
+               padding="same",
+               use_bias=False,
+               kernel_regularizer=l2(weight_decay))(x)
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
+    return x
+
+
+def transition(x, concat_axis, nb_filter, kernel_size=5,
+               dropout_rate=None, weight_decay=1E-4):
+    """Apply BatchNorm, Relu 1x1Conv1D, optional dropout and Maxpooling2D
+    :param x: keras model
+    :param concat_axis: int -- index of contatenate axis
+    :param nb_filter: int -- number of filters
+    :param dropout_rate: int -- dropout rate
+    :param weight_decay: int -- weight decay factor
+    :returns: model
+    :rtype: keras model, after applying batch_norm, relu-conv, dropout, maxpool
+    """
+
+    x = BatchNormalization(axis=concat_axis,
+                           gamma_regularizer=l2(weight_decay),
+                           beta_regularizer=l2(weight_decay))(x)
+    x = Activation('relu')(x)
+    x = Conv1D(nb_filter, kernel_size,
+               kernel_initializer="he_uniform",
+               padding="same",
+               use_bias=False,
+               kernel_regularizer=l2(weight_decay))(x)
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+    x = AveragePooling1D(2, strides=2)(x)
+
+    return x
+
+
+def denseblock(x, concat_axis, nb_layers, nb_filter, growth_rate, dropout_rate=None, weight_decay=1E-4, kernel_size=5):
+    """Build a denseblock where the output of each
+       conv_factory is fed to subsequent ones
+    :param x: keras model
+    :param concat_axis: int -- index of contatenate axis
+    :param nb_layers: int -- the number of layers of conv_
+                      factory to append to the model.
+    :param nb_filter: int -- number of filters
+    :param dropout_rate: int -- dropout rate
+    :param weight_decay: int -- weight decay factor
+    :returns: keras model with nb_layers of conv_factory appended
+    :rtype: keras model
+    """
+
+    list_feat = [x]
+
+    for i in range(nb_layers):
+        x = conv_factory(x, concat_axis, growth_rate, kernel_size,
+                         dropout_rate, weight_decay)
+        list_feat.append(x)
+        x = Concatenate(axis=concat_axis)(list_feat)
+        nb_filter += growth_rate
+
+    return x, nb_filter
+
+
+def denseblock_altern(x, concat_axis, nb_layers, nb_filter, growth_rate, kernel_size=5,
+                      dropout_rate=None, weight_decay=1E-4):
+    """Build a denseblock where the output of each conv_factory
+       is fed to subsequent ones. (Alternative of a above)
+    :param x: keras model
+    :param concat_axis: int -- index of contatenate axis
+    :param nb_layers: int -- the number of layers of conv_
+                      factory to append to the model.
+    :param nb_filter: int -- number of filters
+    :param dropout_rate: int -- dropout rate
+    :param weight_decay: int -- weight decay factor
+    :returns: keras model with nb_layers of conv_factory appended
+    :rtype: keras model
+    * The main difference between this implementation and the implementation
+    above is that the one above
+    """
+
+    for i in range(nb_layers):
+        merge_tensor = conv_factory(x, concat_axis, growth_rate, kernel_size,
+                                    dropout_rate, weight_decay)
+        x = Concatenate(axis=concat_axis)([merge_tensor, x])
+        nb_filter += growth_rate
+
+    return x, nb_filter
+
+
+def DenseNet(x, depth, nb_dense_block, growth_rate, kernel_size=5,
+             nb_filter=16, dropout_rate=None, weight_decay=1E-4):
+    """ Build the DenseNet model
+    :param nb_classes: int -- number of classes
+    :param img_dim: tuple -- (channels, rows, columns)
+    :param depth: int -- how many layers
+    :param nb_dense_block: int -- number of dense blocks to add to end
+    :param growth_rate: int -- number of filters to add
+    :param nb_filter: int -- number of filters
+    :param dropout_rate: float -- dropout rate
+    :param weight_decay: float -- weight decay
+    :returns: keras model with nb_layers of conv_factory appended
+    :rtype: keras model
+    """
+
+    if K.image_dim_ordering() == "th":
+        concat_axis = 1
+    elif K.image_dim_ordering() == "tf":
+        concat_axis = -1
+
+    assert (depth - 4) % 3 == 0, "Depth must be 3 N + 4"
+
+    # layers in each dense block
+    nb_layers = int((depth - 4) / 3)
+
+    # Initial convolution
+    # x = Conv1D(nb_filter, kernel_size,
+    #            kernel_initializer="he_uniform",
+    #            padding="same",
+    #            name="initial_conv1D",
+    #            use_bias=False,
+    #            kernel_regularizer=l2(weight_decay))(input)
+
+    # Add dense blocks
+    for block_idx in range(nb_dense_block - 1):
+        x, nb_filter = denseblock(x, concat_axis, nb_layers,
+                                  nb_filter, growth_rate,
+                                  dropout_rate=dropout_rate,
+                                  weight_decay=weight_decay,
+                                  kernel_size=kernel_size)
+        # add transition
+        x = transition(x, concat_axis, nb_filter, dropout_rate=dropout_rate,
+                       weight_decay=weight_decay,kernel_size=kernel_size)
+
+    # The last denseblock does not have a transition
+    x, nb_filter = denseblock(x, concat_axis, nb_layers, nb_filter, growth_rate, dropout_rate=dropout_rate, kernel_size=kernel_size,
+                              weight_decay=weight_decay)
+
+    x = BatchNormalization(axis=concat_axis,
+                           gamma_regularizer=l2(weight_decay),
+                           beta_regularizer=l2(weight_decay))(x)
+    x = Activation('relu')(x)
+    # out = GlobalAveragePooling1D()(x)
+    # out = Dense(nb_classes,
+    #           activation='softmax',
+    #           kernel_regularizer=l2(weight_decay),
+    #           bias_regularizer=l2(weight_decay))(x)
+
+    # densenet = Model(inputs=[model_input], outputs=[x], name="DenseNet")
+
+
+    return x
