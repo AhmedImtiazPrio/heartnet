f28310c1e5a08b7c1f8378c2c79f4d516f794b70
Md. Istiaq Ansari <ansariistiaq@gmail.com>
2020-01-14 16:50:13 +0600

    reshape_fold function issue solved

--- codes/utils.py
+++ codes/utils.py
@@ -258,11 +258,78 @@ def get_weights(log_name, min_metric=.7, min_epoch=50, verbose=1, log_dir='/medi
 
 
 def reshape_folds(x_train, x_val, y_train, y_val):
-    return np.expand_dims(x_train,axis=-1), np.expand_dims(y_train,axis=-1),\
-           np.expand_dims(x_val,axis=-1) , np.expand_dims(y_val,axis=-1)
+    x1 = np.transpose(x_train[:, :])
 
+    x1 = np.reshape(x1, [x1.shape[0], x1.shape[1], 1])
 
-def load_data(HS, data_dir='../data', _categorical=True, quality=False):
+    y_train = np.reshape(y_train, [y_train.shape[0], 1])
+
+    print(x1.shape)
+    print(y_train.shape)
+
+    v1 = np.transpose(x_val[:, :])
+
+    v1 = np.reshape(v1, [v1.shape[0], v1.shape[1], 1])
+
+    y_val = np.reshape(y_val, [y_val.shape[0], 1])
+
+    print(v1.shape)
+    print(y_val.shape)
+    return x1, y_train, v1 , y_val
+
+     
+def load_data(foldname,fold_dir=None,_categorical=True,quality=False):
+    ## import data
+    if fold_dir is None:
+        fold_dir = '/media/mhealthra2/Data/heart_sound/feature/segmented_noFIR/folds_dec_2018/'
+    else:
+        print(os.path.join(fold_dir,foldname+'.mat'))
+    feat = tables.open_file(os.path.join(fold_dir,foldname + '.mat'))
+    x_train = feat.root.trainX[:]
+    y_train = feat.root.trainY[0, :]
+    q_train = feat.root.trainY[1, :]
+    x_val = feat.root.valX[:]
+    y_val = feat.root.valY[0, :]
+    q_val = feat.root.valY[1, :]
+    train_parts = feat.root.train_parts[:]
+    val_parts = feat.root.val_parts[0, :]
+
+    ############## Relabeling ################
+    
+    for i in range(0, y_train.shape[0]):
+        if y_train[i] == -1:
+            y_train[i] = 0  ## Label 0 for normal 1 for abnormal
+    for i in range(0, y_val.shape[0]):
+        if y_val[i] == -1:
+            y_val[i] = 0
+
+    ############# Parse Database names ########
+
+    train_files = []
+    for each in feat.root.train_files[:][0]:
+        train_files.append(chr(each))
+    print(len(train_files))
+    val_files = []
+    for each in feat.root.val_files[:][0]:
+        val_files.append(chr(each))
+    print(len(val_files))
+
+    ################### Reshaping ############
+
+    x_train, y_train, x_val, y_val = reshape_folds(x_train, x_val, y_train, y_val)
+
+    if _categorical:
+        y_train = to_categorical(y_train, num_classes=2)
+        y_val = to_categorical(y_val, num_classes=2)
+    
+    if quality:
+        return x_train, y_train, train_files, train_parts, q_train, \
+                x_val, y_val, val_files, val_parts, q_val
+    else:
+        return x_train, y_train, train_files, train_parts, \
+                x_val, y_val, val_files, val_parts
+
+def load_dataa(HS, data_dir='../data/feature/folds', _categorical=True, quality=False):
     """
     Helper function to load HS data
     :param HS: data tensor
@@ -271,6 +338,7 @@ def load_data(HS, data_dir='../data', _categorical=True, quality=False):
     :param quality: {True,False} if true, also returns recording quality
     :return: x_train, y_train, train_subset, train_parts, x_val, y_val, val_subset, val_parts
     """
+    print(os.path.join(data_dir,HS+'.mat'))
     feat = loadmat(os.path.join(data_dir,HS+'.mat'))
     x_train = feat['trainX']
     y_train = feat['trainY'][:,0]
